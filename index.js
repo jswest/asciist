c996e913214cb04c751bfa25b6d8b947211f1be2
John West <john.sheffield.west@gmail.com>
2018-07-30 13:26:04 -0400

    Adding the ability to save as an image.

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const path = require('path');
 
+const { createCanvas, loadImage } = require('canvas');
 const d3 = require('d3');
 const Png = require('pngjs').PNG;
 const sharp = require('sharp');
 const yargs = require('yargs');
 
 const args = yargs.argv;
-const chars = [' ', '░', '▒', '▓', '█'].reverse();
+const chars = [' ', '░', '▒', '▓', '█'];
 
-if (args.inverse) {
+const inputPath = args.input[0] === '/' ? args.input : path.join(__dirname, args.input);
+const outputPath = args.output[0] === '/' ? args.output : path.join(__dirname, args.output);
+
+if (args.invert) {
 	chars.reverse();
 }
 
-sharp(args.input)
+sharp(inputPath)
 	.resize(args.size || 100, args.size || 100)
 	.max()
 	.toFormat('png')
@@ -26,19 +31,11 @@ sharp(args.input)
 	.on('parsed', function() {
 		let pixelValues = [];
 		for (let x = 0; x < this.width; x++) {
+			pixelValues[x] = [];
 			for (let y = 0; y < this.height; y++) {
 				let index = (this.width * y + x) << 2;
-
-				const gray =
-					this.data[index] +
-					this.data[index + 1] +
-					this.data[index + 2] / 3;
-
-				this.data[index] = gray;
-				this.data[index + 1] = gray;
-				this.data[index + 2] = gray;
-
-				pixelValues.push(gray);
+				const gray = (this.data[index] + this.data[index + 1] + this.data[index + 2]) / 3;
+				pixelValues[x].push(gray);
 			}
 		}
 		let bins = d3
@@ -53,14 +50,13 @@ sharp(args.input)
 			});
 
 		let output = '';
-		for (let y = 0; y < this.height; y++) {
-			for (let x = 0; x < this.width; x++) {
-				let index = (this.width * y + x) << 2;
+		for (let x = 0; x < pixelValues.length; x++) {
+			for (let y = 0; y < pixelValues[x].length; y++) {
 				for (let i = 0; i < bins.length; i++) {
 					let bin = bins[i];
 					if (
-						this.data[index] >= bin.x0 &&
-						this.data[index] <= bin.x1
+						pixelValues[y][x] >= bin.x0 &&
+						pixelValues[y][x] <= bin.x1
 					) {
 						output += chars[i] + chars[i];
 						break;
@@ -69,5 +65,23 @@ sharp(args.input)
 			}
 			output += '\n';
 		}
-		fs.writeFileSync(args.output, output);
+		if (args.save_image) {
+			const outputValues = output.split('\n');
+			const width = 9.5 * outputValues[0].length;
+			const height = 20 * outputValues.length;
+			const canvas = createCanvas(width, height);
+			const context = canvas.getContext('2d');
+
+			context.font = '16px/16px Menlo';
+			for (let x = 0; x < outputValues.length; x++) {
+				context.fillText(outputValues[x], 0, x * 20 + 20);
+				if (args.log) {
+					console.log(outputValues[x]);
+				}
+			}
+			canvas.createPNGStream().pipe(fs.createWriteStream(outputPath));
+		} else {
+			fs.writeFileSync(outputPath, output);
+		}
+
 	});
