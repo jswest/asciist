7f5c37a84ef4feb2d816ddd38f5b6fa2f0733293
John West <john.sheffield.west@gmail.com>
2018-09-07 10:10:12 -0400

    A whole buch better now.

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const { createCanvas, loadImage } = require('canvas');
-const d3 = require('d3');
-const Png = require('pngjs').PNG;
-const sharp = require('sharp');
-const yargs = require('yargs');
+const { createCanvas, loadImage } = require("canvas");
+const d3 = require("d3");
+const Png = require("pngjs").PNG;
+const sharp = require("sharp");
+const yargs = require("yargs");
 
 const args = yargs.argv;
-const chars = [' ', '░', '▒', '▓', '█'];
+const standardChars = [" ", "░", "▒", "▓", "█"];
+const letterChars = [" ", "l", "o", "E", "N"];
 
-const inputPath = args.input[0] === '/' ? args.input : path.join(__dirname, args.input);
-const outputPath = args.output[0] === '/' ? args.output : path.join(__dirname, args.output);
+const inputPath =
+	args.input[0] === "/" ? args.input : path.join(process.cwd(), args.input);
+const outputPath =
+	args.output[0] === "/"
+		? args.output
+		: path.join(process.cwd(), args.output);
+
+let chars = args.character_set === "letters" ? letterChars : standardChars;
 
 if (args.invert) {
 	chars.reverse();
@@ -22,20 +29,24 @@ if (args.invert) {
 sharp(inputPath)
 	.resize(args.size || 100, args.size || 100)
 	.max()
-	.toFormat('png')
+	.toFormat("png")
 	.pipe(
 		new Png({
-			filterType: 4,
+			filterType: 4
 		})
 	)
-	.on('parsed', function() {
+	.on("parsed", function() {
 		let pixelValues = [];
-		for (let x = 0; x < this.width; x++) {
-			pixelValues[x] = [];
-			for (let y = 0; y < this.height; y++) {
+		for (let y = 0; y < this.height; y++) {
+			pixelValues[y] = [];
+			for (let x = 0; x < this.width; x++) {
 				let index = (this.width * y + x) << 2;
-				const gray = (this.data[index] + this.data[index + 1] + this.data[index + 2]) / 3;
-				pixelValues[x].push(gray);
+				const gray =
+					(this.data[index] +
+						this.data[index + 1] +
+						this.data[index + 2]) /
+					3;
+				pixelValues[y].push(gray);
 			}
 		}
 		let bins = d3
@@ -49,9 +60,9 @@ sharp(inputPath)
 				return a.x0 < b.x0 ? 1 : -1;
 			});
 
-		let output = '';
-		for (let x = 0; x < pixelValues.length; x++) {
-			for (let y = 0; y < pixelValues[x].length; y++) {
+		let output = "";
+		for (let y = 0; y < pixelValues.length; y++) {
+			for (let x = 0; x < pixelValues[y].length; x++) {
 				for (let i = 0; i < bins.length; i++) {
 					let bin = bins[i];
 					if (
@@ -63,28 +74,28 @@ sharp(inputPath)
 					}
 				}
 			}
-			output += '\n';
+			output += "\n";
 		}
 		if (args.save_as_image) {
-			const outputValues = output.split('\n');
+			const outputValues = output.split("\n");
 			const width = 9.5 * outputValues[0].length;
 			const height = 20 * outputValues.length;
 			const canvas = createCanvas(width, height);
-			const context = canvas.getContext('2d');
+			const context = canvas.getContext("2d");
 
-			context.fillStyle = '#ffffff';
+			context.fillStyle = "#ffffff";
 			context.fillRect(0, 0, width, height);
 
-			context.font = '16px/16px Menlo';
-			for (let x = 0; x < outputValues.length; x++) {
-				context.fillText(outputValues[x], 0, x * 20 + 20);
+			context.font = "16px/16px Menlo";
+			for (let y = 0; y < outputValues.length; y++) {
+				context.fillStyle = "#000000";
+				context.fillText(outputValues[y], 0, y * 20 + 20);
 				if (args.log) {
-					console.log(outputValues[x]);
+					console.log(outputValues[y]);
 				}
 			}
 			canvas.createPNGStream().pipe(fs.createWriteStream(outputPath));
 		} else {
 			fs.writeFileSync(outputPath, output);
 		}
-
 	});
